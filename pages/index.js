c2f84a05a67773638ebb32186d5252e14169723d
shantorock <shanto140318@gmail.com>
2022-05-03 20:14:19 +0600

    test build for vercel app

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,18 @@ import Head from 'next/head'
 import { getTopRated, getTrending, getUpcoming } from '../api/getAllData'
 import Category from '../components/category/Category'
 import styles from '../styles/Home.module.css'
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const trendings = await getTrending(1)
   const toprated = await getTopRated(1)
   const upComing = await getUpcoming(1)
-
+  const response = await Promise.all([trendings, toprated, upComing])
   return {
     props: {
-      trendings,
-      toprated,
-      upComing,
+      trendings: response[0],
+      toprated: response[1],
+      upComing: response[2],
     },
-    // revalidate: 60 * 5, // In seconds
+    revalidate: 60 * 5, // In seconds
   }
 }
 
